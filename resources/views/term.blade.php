ce8b49a54d583bc973a8f751ed8d9c3ecab4823c
eddymens <primerossgh@gmail.com>
2021-11-03 11:16:03 +0100

    add github link

--- resources/views/term.blade.php
+++ resources/views/term.blade.php
@@ -32,6 +32,7 @@
                 {!! $definition !!}
                 @include('definitionHeading')
             </div>
+              <a href="https://github.com/EDDYMENS/coding-terms-markdown" target="__blank" class="center-it"><b>Github</b></a>
           </div>
           <div class="col-1"></div>
           </article>
